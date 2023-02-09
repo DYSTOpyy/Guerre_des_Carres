9da4b2d93c77fb424f4564c1e4fbf9b0a0dbdde7
DYSTO_pyy <bcd.saltel@gmail.com>
2023-02-09 15:33:20 +0100

    interface de choix de couleur en beta, index.js supprimé, README actualisé et début de la liste d'utilisateurs

--- README.md
+++ README.md
@@ -8,8 +8,34 @@ Tableau de pixels qui contient dans chaque case une classe avec
 - la date
 - la couleur du pixel
 
-sauvegarder le nom du gars dans les cookies
+===============
+A FAIRE
 
-choix de couleurs limités ? ou sans limite ?
+sauvegarder le nom du gars dans le dico du js, pas dans les cookies car si il le modifie c'est le bordel
+choix de couleurs limités
 
-sauvegarder le tableau pour éviter qu'il disparaisse ?
+- finir l'interface de choix des couleurs
+
+    - choisir un bloc, choisir la couleur et hop le bloc change OU L'INVERSE 
+
+        - choisir un bloc === mettre son contour ! et enlever le contout de l'ancien !
+
+ACTUELLEMENT : en sélectionnant une couleur sur le côté, je peut changer la couleur du truc blanc.
+Il faut finir l'interface pour la rendre opérationelle (et donc savoir comment maxime gère ses pixels car moi je me base sur les id des boutons)
+cad choisir le bloc, choisir la couleur et ça la modifie
+
+- trouver une solution pour partager avec les nouveaux arrivants
+   dès qu'un nouveau arrive on récupère l'état actuel du jeu sur le premier du tableau, puis on fout le nouveau dans le tableau
+
+- stocker l'état du tableau
+
+- page d'accueil 
+
+nouveau : donner pseudo + cookie d'auth
+deja vu : rien faire
+--> passe au jeux
+
+- cookie de session (sauf si existant)
+    un tableau avec tous les utilisateurs, une file (le premier est le dernier arrivé)
+
+- pseudo
--- test.js
+++ test.js
@@ -6,31 +6,40 @@ var http = require('http').createServer(app);
 var io = require("socket.io")(http);
 const { dirname } = require('path');
 
-//  appeler le dossier publique
-app.use(express.static("public"));
+// inclure le dossier public !! pour tout ce qui est static (css, image) NOTE : il y a pas le '/' à la fin de public, il faut donc le mettre au début de tous les liens (ex : href="/css/style.css")
+app.use(express.static(__dirname + '/public'));
 
+// envoyer la page d'accueil lorsque l'on va sur /
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
 }
 );
 
+// envoyer une autre page lorsque l'on va sur /basic.html
 app.get('/basic.html', function(req,res){
   res.sendFile(__dirname + '/basic.html');
 }
 );
 
+// lorsque quelqu'un se connecte au socket
 io.on("connection", function (socket) {
     console.log("Made socket connection");
-
-    socket.on("checkedTrue", (data) => {
-      socket.emit("change", "basic.html", data);
-      // io.emit("change", ("test", data));
+    var room = io.sockets.adapter.rooms['/'];
+    console.log(room);
 
 
+    socket.on("checkedTrue", (data, idCarreAChanger) => {
+      io.emit("change", data);
+
+    // lors de la déconnexion
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
 
   });
 
 });
 
+// lancer le serveur en localhost:80 avec la commande "node index.js" (pourquoi j'ai remplacé server.listen par ça ?)
 http.listen(80)
 
