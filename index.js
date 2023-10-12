b205c71dff8bb2bbd04944dfe3f079f0d179bb9c
maxlem24 <lemaitre.maxime2004@gmail.com>
2023-10-12 16:22:56 +0200

    Fix Faille Majeure n°2  (Merci a tous)

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ io.on("connection", (socket) => {     // socket.io
 
 	socket.on("newUser", (username, id) => {
 		let valide = true;        // test si pseudo déjà existant
+		username = username.match(/[\w]+/)
 		for (let name of users.values()) {
 			if (name === username) { valide = false; }
 		}
